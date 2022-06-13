740d0d1f0670fadb66fbe172e02218d3978ebaea
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-06-13 17:53:27 -0300

    Home
    Estilizada a homepage

--- myapp/src/pages/Home.jsx
+++ myapp/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import styles from './Home.module.css'
+
+import savings from '../img/pngwing.com.png'
+import LinkButton from '../components/layout/LinkButton'
 
 const Home = () => {
     return (
-        <div>
-            HOME
-        </div>
+        <section className={styles.home_container}>
+            <h1>Bem vindo ao <span>Projetando</span></h1>
+            <p>Não perca tempo! Gerencie seus projetos agora mesmo.</p>
+            <LinkButton to="/projects" text="Criar Meu Projeto" />
+            <img src={savings} alt="Economize deu dinheiro"></img>
+        </section>
     )
 }
 
