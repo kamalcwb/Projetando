d86cacd4f624ca852d1ef547ddf8e07655735ed7
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-06-14 17:10:09 -0300

    configurando api fake

--- myapp/src/components/project/ProjectForm.jsx
+++ myapp/src/components/project/ProjectForm.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 const ProjectForm = ({ btnText }) => {
+    const [categories, setCategories] = useState([])
+
+    // fetch("http://localhost:5000/categories", {
+    //     method: "GET",
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // })
+    //     .then((resp) => resp.json())
+    //     .then((data) => {
+    //         setCategories(data)
+    //     })
+    //     .catch((err) => console.log(err))
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
     return (
         <form className={styles.form}>
             <Input
@@ -21,7 +49,7 @@ const ProjectForm = ({ btnText }) => {
             />
 
 
-            <Select name="category_id" text="Selecione a categoria" />
+            <Select name="category_id" text="Selecione a categoria" options={categories} />
 
             <SubmitButton text={btnText} />
         </form>
