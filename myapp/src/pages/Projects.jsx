5cbf5185ebe7e4125485c24d24f9a9f4373ba0fe
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-06-18 19:44:43 -0300

    Pg Projects
    Dado inicio a configuração da página Projects

--- myapp/src/pages/Projects.jsx
+++ myapp/src/pages/Projects.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import Message from '../components/layout/Message'
+import styles from './Projects.module.css'
+import Container from '../components/layout/Container'
+import LinkButton from '../components/layout/LinkButton'
 
 const Projects = () => {
 
@@ -11,9 +14,15 @@ const Projects = () => {
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
             {message && <Message msg={message} type="success" />}
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
         </div>
     )
 }
