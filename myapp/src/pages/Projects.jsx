d24c4409c2dd38a9ad0620b8ef497ceebe6c8854
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-06-19 20:22:10 -0300

    Card de projetos
    adicionado e estilizado o card de projetos

--- myapp/src/pages/Projects.jsx
+++ myapp/src/pages/Projects.jsx
@@ -4,14 +4,30 @@ import Message from '../components/layout/Message'
 import styles from './Projects.module.css'
 import Container from '../components/layout/Container'
 import LinkButton from '../components/layout/LinkButton'
+import ProjectCard from '../components/project/ProjectCard'
+import { useState, useEffect } from 'react'
 
 const Projects = () => {
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = '';
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(resp => resp.json())
+            .then(data => {
+                setProjects(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
 
     return (
         <div className={styles.project_container}>
@@ -21,7 +37,16 @@ const Projects = () => {
             </div>
             {message && <Message msg={message} type="success" />}
             <Container customClass="start">
-                <p>Projetos</p>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    ))}
             </Container>
         </div>
     )
