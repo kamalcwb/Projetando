06cd327905912f5dc611736700dc1525e0163789
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-06-24 16:50:12 -0300

    Update Project.jsx
    Area de serviços

--- myapp/src/pages/Project.jsx
+++ myapp/src/pages/Project.jsx
@@ -15,6 +15,7 @@ const Project = () => {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -37,7 +38,12 @@ const Project = () => {
         setShowProjectForm(!showProjectForm)
     }
 
+    const toggleServiceForm = () => {
+        setShowServiceForm(!showServiceForm)
+    }
+
     const editPost = (project) => {
+        setMessage('')
         if (project.budget < project.cost) {
             setMessage('Orçamento insuficiente!')
             setType('error')
@@ -93,6 +99,21 @@ const Project = () => {
                             </div>
                         )}
                     </div>
+                    <div className={styles.service_form_container}>
+                        <h2>Adicione um serviço:</h2>
+                        <button onClick={toggleServiceForm} className={styles.btn}>
+                            {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+                        </button>
+                        <div className={styles.project_info}>
+                            {showServiceForm && <div>formulário de serviço</div>
+
+                            }
+                        </div>
+                    </div>
+                    <h2>Serviços</h2>
+                    <Container customClass="start">
+                        <p>Serviços disponiveis</p>
+                    </Container>
                 </Container>
             </div>
             : <Loading />} </div>
